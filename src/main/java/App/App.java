d3e430bec496ec6dd7dd30442d55d6786fe5b0c6
abcarrillo <albertobarrosocarrillo@gmail.com>
2020-11-09 18:42:15 +0000

    terminado los procedimientos, falta access

--- src/main/java/App/App.java
+++ src/main/java/App/App.java
@@ -6,13 +6,12 @@ import Modelo.AccesoBD;
 
 public class App {
 	private static boolean exit = false;
-	//private static String eleccion = "";
+	// private static String eleccion = "";
 	private static Scanner sc = new Scanner(System.in);
 
 	private static void mostrarMenu() {
 		try {
-			
-			
+
 			System.out.println("-------------------------");
 			System.out.println("Bienvenido a la gestión de 'bdfutbol'.");
 			System.out.println("¿Qué quiere hacer? \n");
@@ -27,79 +26,77 @@ public class App {
 			eleccion = sc.nextLine();
 
 			switch (eleccion) {
-				case "1":
-					mostrarSubmenu("listado");
-					break;
-				case "2":
-					mostrarSubmenu("insercion");
-					break;
-				case "3":
-					mostrarSubmenu("borrado");
-					break;
-				case "4":
-					mostrarSubmenu("modificacion");
-					break;
-				case "5":
-					mostrarSubmenuProcedimientos();
-					break;
-				case "6": {
-					exit = true;
-					System.out.println("GRACIAS");
-					break;
-				}
-				default:
-					exit = true;
-					System.out.println("GRACIAS");
-					break;
+			case "1":
+				mostrarSubmenu("listado");
+				break;
+			case "2":
+				mostrarSubmenu("insercion");
+				break;
+			case "3":
+				mostrarSubmenu("borrado");
+				break;
+			case "4":
+				mostrarSubmenu("modificacion");
+				break;
+			case "5":
+				mostrarSubmenuProcedimientos();
+				break;
+			case "6": {
+				exit = true;
+				System.out.println("GRACIAS");
+				break;
 			}
-			
-		}catch(Exception ex) {
+			default:
+				exit = true;
+				System.out.println("GRACIAS");
+				break;
+			}
+
+		} catch (Exception ex) {
 			System.out.println("Volvio a ocurrir");
 		}
-		
-		
+
 	}
 
 	private static void mostrarSubmenuProcedimientos() {
-		
+
 		System.out.println("");
 		System.out.println("Procedimientos disponibles: ");
 		System.out.println("1: Insertar un equipo");
 		System.out.println("2: Listar contratos según DNI");
 		System.out.println("3: Futbolistas activos en un equipo");
-		System.out.println("4: Cantidad de meses jugados por DNI");
+		System.out.println("4: Cantidad de meses jugados totales por un jugador (DNI/NIE)");
 		System.out.println("5: Cancelar operación");
 		String eleccion = sc.nextLine();
 
 		switch (eleccion) {
-			case "1": {
-				mostrarSubmenu("procedimiento_insertar");
-				break;
-			}
-			case "2": {
-	
-				break;
-			}
-			case "3": {
-	
-				break;
-			}
-			case "4": {
-	
-				break;
-			}
-			case "5": {
-	
-				break;
-			}
-			default:
-				break;
+		case "1": {
+			mostrarSubmenu("procedimiento_insertar");
+			break;
 		}
-		
+		case "2": {
+			mostrarSubmenu("procedimiento_contratos_dni");
+			break;
+		}
+		case "3": {
+			mostrarSubmenu("procedimiento_contratos_activos");
+			break;
+		}
+		case "4": {
+			mostrarSubmenu("procedimiento_meses_totales");
+			break;
+		}
+		case "5": {
+			break;
+		}
+		default:
+			break;
+		}
+
 	}
 
 	private static void mostrarSubmenu(String operacion) {
-		
+
 		System.out.println("");
 		System.out.println("¿Que base de datos?");
 		System.out.println("1: MySQL");
@@ -109,49 +106,101 @@ public class App {
 		String eleccion = sc.nextLine();
 
 		switch (eleccion) {
-			case "1": {
-				switch (operacion) {
-					case "listado": {
-						AccesoBD.listarEquiposMySQL();
-						break;
-					}
-					case "insercion": {
-						AccesoBD.insertarEquipoMySQL();
-						break;
-					}
-					case "borrado": {
-						AccesoBD.listarEquiposMySQL();
-						System.out.println("¿Qué equipo quieres borrar? (0 para cancelar)");
-						String cod = sc.nextLine();
-						AccesoBD.eliminarEquipoMySQL(cod);
-						break;
-					}
-					case "modificacion": {
-						AccesoBD.listarEquiposMySQL();
-						System.out.println("¿Qué equipo quieres modificar? \n");
-						String cod = sc.nextLine();
-						AccesoBD.modificarEquipoMySQL(cod);
-						break;
-					}
-					case "procedimiento_insertar": {
-						AccesoBD.insertarEquipoProcedimientoMySQL();
-						break;
-					}
+		case "1": {
+			switch (operacion) {
+				case "listado": {
+					AccesoBD.listarEquiposMySQL();
+					break;
 				}
-				break;
-			}
-			case "2": {
+				case "insercion": {
+					AccesoBD.insertarEquipoMySQL();
+					break;
+				}
+				case "borrado": {
+					AccesoBD.listarEquiposMySQL();
+					System.out.println("¿Qué equipo quieres borrar? (0 para cancelar)");
+					String cod = sc.nextLine();
+					AccesoBD.eliminarEquipoMySQL(cod);
+					break;
+				}
+				case "modificacion": {
+					AccesoBD.listarEquiposMySQL();
+					System.out.println("¿Qué equipo quieres modificar? \n");
+					String cod = sc.nextLine();
+					AccesoBD.modificarEquipoMySQL(cod);
+					break;
+				}
+				case "procedimiento_insertar": {
+					AccesoBD.insertarEquipoProcedimientoMySQL();
+					break;
+				}
 	
-				break;
+				case "procedimiento_contratos_dni": {
+					AccesoBD.contratosPorDNIProcedimientoMySQL();
+					break;
+				}
+				case "procedimiento_contratos_activos":{
+					AccesoBD.jugadoresActivosProcedimientoMySQL();
+					break;
+				}
+				case "procedimiento_meses_totales":{
+					AccesoBD.mesesTotalesProcedimientoMySQL();
+				}
 			}
-			case "3": {
-				break;
+			break;
+		}
+		case "2": {
+
+			break;
+		}
+		case "3": {
+			switch (operacion) {
+				case "listado": {
+					AccesoBD.listarEquiposSqlServer();
+					break;
+				}
+				case "insercion": {
+					AccesoBD.insertarEquipoSQLServer();
+					break;
+				}
+				case "borrado": {
+					AccesoBD.listarEquiposSqlServer();
+					System.out.println("¿Qué equipo quieres borrar? (0 para cancelar)");
+					String cod = sc.nextLine();
+					AccesoBD.eliminarEquipoSQLServer(cod);
+					break;
+				}
+				case "modificacion": {
+					AccesoBD.listarEquiposSqlServer();
+					System.out.println("¿Qué equipo quieres modificar? \n");
+					String cod = sc.nextLine();
+					AccesoBD.modificarEquipoSQLServer(cod);
+					break;
+				}
+				case "procedimiento_insertar": {
+					AccesoBD.insertarEquipoProcedimientoSQLServer();
+					break;
+				}
+				
+				case "procedimiento_contratos_dni":{
+					AccesoBD.contratosPorDNIProcedimientoSQLServer();
+					break;
+				}	
+				case "procedimiento_contratos_activos":{
+					AccesoBD.jugadoresActivosProcedimientoSQLServer();
+					break;
+				}
+				case "procedimiento_meses_totales":{
+					AccesoBD.mesesTotalesProcedimientoSQLServer();
+				}
 			}
-			case "4": {
-				break;
-			}
-			default:
-				break;
+			break;
+		}
+		case "4": {
+			break;
+		}
+		default:
+			break;
 		}
 	}
 
@@ -160,6 +209,6 @@ public class App {
 		while (exit == false) {
 			mostrarMenu();
 		}
-		
+
 	}
 }
--- src/main/java/Modelo/AccesoBD.java
+++ src/main/java/Modelo/AccesoBD.java
@@ -3,15 +3,21 @@ package Modelo;
 import java.sql.*;
 import java.util.Scanner;
 
+import com.mysql.cj.xdevapi.Type;
+
 public class AccesoBD {
 
-	private static String url = "jdbc:mysql://localhost:3306/bdfutbol";
-	private static String user = "root";
-	private static String pass = "";
+	private static String urlSQL = "jdbc:mysql://localhost:3306/bdfutbol";
+	private static String userSQL = "root";
+	private static String passSQL = "";
+	private static String urlSQLServer = "jdbc:sqlserver://DESKTOP-OB3TRNJ;DataBaseName=bdFutbol";
+	private static String userSQLServer = "sa";
+	private static String passSQLServer = "2020informatica";
+	
 
 	public static void listarEquiposMySQL() {
 		try {
-			Connection con = DriverManager.getConnection(url, user, pass);
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
 
 			String sql = "SELECT * FROM equipos INNER JOIN ligas ON ligas.codLiga = equipos.codLiga";
 			PreparedStatement consulta = con.prepareStatement(sql);
@@ -36,12 +42,81 @@ public class AccesoBD {
 		}
 
 	}
+	
+	
+	public static void listarEquiposSqlServer() {
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+
+			String sql = "SELECT * FROM equipos INNER JOIN ligas ON ligas.codLiga = equipos.codLiga";
+			PreparedStatement consulta = con.prepareStatement(sql);
+
+			ResultSet resultado = consulta.executeQuery();
+			System.out.println("");
+
+			while (resultado.next()) {
+				String codEquipo = resultado.getString("codEquipo");
+				String nomEquipo = resultado.getString("nomEquipo");
+				String nombreLiga = resultado.getString("nomLiga");
+				String localidad = resultado.getString("localidad");
+				int internacional = resultado.getInt("internacional");
+
+				System.out.println("- " + codEquipo + " | " + nomEquipo + " | " + nombreLiga + " | " + localidad + " | "
+						+ internacional);
+			}
+			System.out.println("");
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
 
 	public static void modificarEquipoMySQL(String codEquipo) {
 		Scanner sc = new Scanner(System.in);
 		try {
-			Connection con = DriverManager.getConnection(url, user, pass);
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
 			String sql = "UPDATE `equipos` SET nomEquipo=?, codLiga=?, localidad=?, internacional=? WHERE codEquipo = "
+					+ codEquipo;
+			PreparedStatement consulta = con.prepareStatement(sql);
+
+			String sql2 = "SELECT * FROM equipos WHERE codEquipo = " + codEquipo;
+			PreparedStatement modificar = con.prepareStatement(sql2);
+			ResultSet resultado = modificar.executeQuery();
+
+			if (resultado.next()) {
+				System.out.println("Nombre de equipo (Actual: " + resultado.getString("nomEquipo") + " )");
+				String nomEquipo = sc.nextLine();
+				System.out.println("Codigo de liga (Actual: " + resultado.getString("codLiga") + " )");
+				String codLiga = sc.nextLine();
+				System.out.println("Localidad (Actual: " + resultado.getString("localidad") + " )");
+				String localidad = sc.nextLine();
+				System.out.println("Internacional (si o no) (Actual: " + resultado.getString("internacional") + " )");
+				String internacionalString = sc.nextLine();
+				boolean internacional = false;
+
+				if (internacionalString.toLowerCase().equals("si")) {
+					internacional = true;
+				}
+
+				consulta.setString(1, nomEquipo);
+				consulta.setString(2, codLiga);
+				consulta.setString(3, localidad);
+				consulta.setBoolean(4, internacional);
+			}
+
+			consulta.executeUpdate();
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public static void modificarEquipoSQLServer(String codEquipo) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+			String sql = "UPDATE equipos SET nomEquipo=?, codLiga=?, localidad=?, internacional=? WHERE codEquipo = "
 					+ codEquipo;
 			PreparedStatement consulta = con.prepareStatement(sql);
 
@@ -80,7 +155,72 @@ public class AccesoBD {
 	public static void eliminarEquipoMySQL(String codEquipo) {
 		Scanner sc = new Scanner(System.in);
 		try {
-			Connection con = DriverManager.getConnection(url, user, pass);
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
+
+			String sqlEquipo = "SELECT * FROM equipos WHERE codEquipo = " + codEquipo;
+			PreparedStatement consultaEquipo = con.prepareStatement(sqlEquipo);
+			ResultSet resultadoEquipo = consultaEquipo.executeQuery();
+
+			String sqlContratos = "SELECT * FROM contratos WHERE codEquipo = " + codEquipo;
+			PreparedStatement consultaContratos = con.prepareStatement(sqlContratos);
+			ResultSet resultadoContratos = consultaContratos.executeQuery();
+
+			boolean borrado = false;
+			if (resultadoEquipo.next()) {
+				if (resultadoContratos.isBeforeFirst() == false) {
+					System.out.println("¿Seguro que quiere borrar este equipo? (si o no)");
+					String eleccion = sc.nextLine();
+
+					if (eleccion.toLowerCase().equals("si")) {
+						borrado = true;
+					}
+				} else {
+					System.out.println("Si quiere borrar este equipo, debe borrar estos contratos: \n");
+					
+					while (resultadoContratos.next()) {
+
+						String codContrato = resultadoContratos.getString("codcontrato");
+						String coddnionie = resultadoContratos.getString("coddnionie");
+						String fechaInicio = resultadoContratos.getString("fechaInicio");
+						String fechaFin = resultadoContratos.getString("fechaFin");
+						String precioanual = resultadoContratos.getString("precioanual");
+						String preciorecision = resultadoContratos.getString("preciorecision");
+
+						System.out.println(" - " + codContrato + " | " + coddnionie + " | " + fechaInicio + " | "
+								+ fechaFin + " | " + precioanual + " | " + preciorecision);
+					}
+					System.out.println("");
+					System.out.println("¿Seguro que quiere borrar este equipo y estos contratos? (si o no)");
+					String eleccion = sc.nextLine();
+
+					if (eleccion.toLowerCase().equals("si")) {
+						borrado = true;
+					}
+				}
+
+			}
+
+			if (borrado == true) {
+				String sqlBorradoContratos = "DELETE FROM contratos WHERE codEquipo = " + codEquipo;
+				PreparedStatement borradoContratos = con.prepareStatement(sqlBorradoContratos);
+				borradoContratos.executeUpdate();
+
+				String sqlBorradoEquipo = "DELETE FROM equipos WHERE codEquipo = " + codEquipo;
+				PreparedStatement borradoEquipo = con.prepareStatement(sqlBorradoEquipo);
+				borradoEquipo.executeUpdate();
+			}
+
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void eliminarEquipoSQLServer(String codEquipo) {
+		Scanner sc = new Scanner(System.in);
+		try {
+			
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
 
 			String sqlEquipo = "SELECT * FROM equipos WHERE codEquipo = " + codEquipo;
 			PreparedStatement consultaEquipo = con.prepareStatement(sqlEquipo);
@@ -145,8 +285,42 @@ public class AccesoBD {
 
 		Scanner sc = new Scanner(System.in);
 		try {
-			Connection con = DriverManager.getConnection(url, user, pass);
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
 			String sql = "INSERT INTO `equipos`(`nomEquipo`, `codLiga`, `localidad`, `internacional`) VALUES (?,?,?,?)";
+			PreparedStatement consulta = con.prepareStatement(sql);
+
+			System.out.println("Nombre de equipo: ");
+			String nomEquipo = sc.nextLine();
+			System.out.println("Codigo de liga: ");
+			String codLiga = sc.nextLine();
+			System.out.println("Localidad: ");
+			String localidad = sc.nextLine();
+			System.out.println("Internacional (si o no): ");
+			String internacionalString = sc.nextLine();
+			boolean internacional = false;
+
+			if (internacionalString.toLowerCase().equals("si")) {
+				internacional = true;
+			}
+
+			consulta.setString(1, nomEquipo);
+			consulta.setString(2, codLiga);
+			consulta.setString(3, localidad);
+			consulta.setBoolean(4, internacional);
+
+			consulta.executeUpdate();
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void insertarEquipoSQLServer() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+			String sql = "INSERT INTO equipos (nomEquipo, codLiga, localidad, internacional) VALUES (?,?,?,?)";
 			PreparedStatement consulta = con.prepareStatement(sql);
 
 			System.out.println("Nombre de equipo: ");
@@ -179,7 +353,7 @@ public class AccesoBD {
 
 		Scanner sc = new Scanner(System.in);
 		try {
-			Connection con = DriverManager.getConnection(url, user, pass);
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
 			CallableStatement consulta = con.prepareCall("call actividad2(?, ?, ?, ?, ?, ?)");
 
 			System.out.println("Nombre de equipo: ");
@@ -208,6 +382,230 @@ public class AccesoBD {
 			System.out.println("La liga insertada existe: " + consulta.getString(5) + " | Estado de inserción: "
 					+ consulta.getString(6) + " \n");
 
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public static void insertarEquipoProcedimientoSQLServer() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+			CallableStatement consulta = con.prepareCall("exec dbo.InsertaEquipo @nomEquipo = ?, @codLiga = ?, " +
+			"@localidad = ?, @internacional = ?, @existeliga = ?,"
+			+ "	@insercion = ?");
+
+			System.out.println("Nombre de equipo: ");
+			String nomEquipo = sc.nextLine();
+			System.out.println("Codigo de liga: ");
+			String codLiga = sc.nextLine();
+			System.out.println("Localidad: ");
+			String localidad = sc.nextLine();
+			System.out.println("Internacional (si o no): ");
+			String internacionalString = sc.nextLine();
+			boolean internacional = false;
+
+			consulta.setString(1, nomEquipo);
+			consulta.setString(2, codLiga);
+			consulta.setString(3, localidad);
+			consulta.setBoolean(4, internacional);
+			consulta.registerOutParameter(5, Types.TINYINT);
+			consulta.registerOutParameter(6, Types.TINYINT);
+
+			if (internacionalString.toLowerCase().equals("si")) {
+				internacional = true;
+			}
+
+			consulta.execute();
+			System.out.println("");
+			System.out.println("La liga insertada existe: " + consulta.getString(5) + " | Estado de inserción: "
+					+ consulta.getString(6) + " \n");
+
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void contratosPorDNIProcedimientoMySQL() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
+			CallableStatement consulta = con.prepareCall("CALL actividad1(?);");
+
+			System.out.println("DNI del jugador: ");
+			String dni = sc.nextLine();
+			
+			consulta.setString(1, dni);
+
+			ResultSet resultado = consulta.executeQuery();
+			System.out.println("");
+			while (resultado.next()) {
+	            String codcontrato = resultado.getString("codcontrato");
+	            String nomEquipo = resultado.getString("nomEquipo");
+	            String fechaInicio = resultado.getString("fechaInicio");
+	            String fechaFin = resultado.getString("fechaFin");
+	            String precioanual = resultado.getString("precioanual");
+	            String preciorecision = resultado.getString("preciorecision");    
+	            System.out.println("- "+ codcontrato+" | "+nomEquipo+" | "+fechaInicio+" | "+fechaFin+" | "+precioanual+" | "+preciorecision);
+	        }
+
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void contratosPorDNIProcedimientoSQLServer() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+			CallableStatement consulta = con.prepareCall("EXEC dbo.ListaContratos @DNINIE = ?");
+
+			System.out.println("DNI del jugador: ");
+			String dni = sc.nextLine();
+			
+			consulta.setString(1, dni);
+
+			ResultSet resultado = consulta.executeQuery();
+			System.out.println("");
+			while (resultado.next()) {
+	            String codcontrato = resultado.getString("codcontrato");
+	            String nomEquipo = resultado.getString("nomEquipo");
+	            String fechaInicio = resultado.getString("fechaInicio");
+	            String fechaFin = resultado.getString("fechaFin");
+	            String precioanual = resultado.getString("precioanual");
+	            String preciorecision = resultado.getString("preciorecision");    
+	            System.out.println("- "+ codcontrato+" | "+nomEquipo+" | "+fechaInicio+" | "+fechaFin+" | "+precioanual+" | "+preciorecision);
+	        }
+
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public static void jugadoresActivosProcedimientoMySQL() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
+			CallableStatement consulta = con.prepareCall(" CALL `actividad3`(?, ?, ?, ?, ?)");
+			
+			System.out.println("A continuacion, introduzca el codigo de equipo y el precio anual y de recision del contrato.");
+			System.out.println("Saldran en pantalla los contratos activos de ese equipos y los MENORES a los precios anuales y de recision introducidos.");
+
+			System.out.println("Codigo de equipo: ");
+			String codEquipo = sc.nextLine();
+			System.out.println("Precio anual: ");
+			String precioanual = sc.nextLine();
+			System.out.println("Precio recision: ");
+			String preciorecision = sc.nextLine();
+			
+			consulta.setString(1, codEquipo);
+			consulta.setString(2, precioanual);
+			consulta.setString(3, preciorecision);
+
+			consulta.execute();
+			System.out.println("");
+			System.out.println("Hay esta cantidad de contratos activos en el equipo introducido: " + consulta.getString(4) + " | De esos, hay esta cantidad con precios menores a los establecidos: "
+					+ consulta.getString(5) + " \n");
+
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public static void jugadoresActivosProcedimientoSQLServer() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+			CallableStatement consulta = con.prepareCall("exec dbo.FutbolistasActivoEquipo @codEquipo = ?, @precioAnual = ?, " +
+					"@precioRecision = ?, @futbolistasActivos = ?, @futbolistasActivos2 = ?");
+			
+			System.out.println("A continuacion, introduzca el codigo de equipo y el precio anual y de recision del contrato.");
+			System.out.println("Saldran en pantalla los contratos activos de ese equipos y los MENORES a los precios anuales y de recision introducidos.");
+
+			System.out.println("Codigo de equipo: ");
+			String codEquipo = sc.nextLine();
+			System.out.println("Precio anual: ");
+			String precioanual = sc.nextLine();
+			System.out.println("Precio recision: ");
+			String preciorecision = sc.nextLine();
+			
+			consulta.setString(1, codEquipo);
+			consulta.setString(2, precioanual);
+			consulta.setString(3, preciorecision);
+			consulta.registerOutParameter(4, Types.INTEGER);
+			consulta.registerOutParameter(5, Types.INTEGER);
+
+			consulta.execute();
+			System.out.println("");
+			System.out.println("Hay esta cantidad de contratos activos en el equipo introducido: " + consulta.getString(4) + " | De esos, hay esta cantidad con precios menores a los establecidos: "
+					+ consulta.getString(5) + " \n");
+
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	public static void mesesTotalesProcedimientoMySQL() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQL, userSQL, passSQL);
+
+			String sql = "SELECT actividad4(?) as meses";
+			PreparedStatement consulta = con.prepareStatement(sql);
+			System.out.println("DNI del jugador: ");
+			String dni = sc.nextLine();
+			
+			consulta.setString(1, dni);
+
+			ResultSet resultado = consulta.executeQuery();
+			if(resultado.next()) {
+				System.out.println("");
+				System.out.println("El jugador ha estado en activo un total de " + resultado.getString(1) + " meses");
+
+			}
+			
+			con.close();
+		} catch (Exception ex) {
+			System.out.println(ex.getMessage());
+		}
+	}
+	
+	
+	public static void mesesTotalesProcedimientoSQLServer() {
+
+		Scanner sc = new Scanner(System.in);
+		try {
+			Connection con = DriverManager.getConnection(urlSQLServer, userSQLServer, passSQLServer);
+
+			String sql = "SELECT dbo.MesesSegunDNIoNIE(?) as meses";
+			PreparedStatement consulta = con.prepareStatement(sql);
+			System.out.println("DNI del jugador: ");
+			String dni = sc.nextLine();
+			
+			consulta.setString(1, dni);
+
+			ResultSet resultado = consulta.executeQuery();
+			if(resultado.next()) {
+				System.out.println("");
+				System.out.println("El jugador ha estado en activo un total de " + resultado.getString(1) + " meses");
+
+			}
+			
 			con.close();
 		} catch (Exception ex) {
 			System.out.println(ex.getMessage());
